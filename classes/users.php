c0bd97e417435ce43e8f8d8fd6df1d58ae871e39
enkelaveselaj <ev68484@ubt-uni.net>
2024-12-20 19:59:06 +0100

    added get and set methods in users.php class

--- classes/users.php
+++ classes/users.php
@@ -21,4 +21,46 @@ class Users extends dbConnect {
         $this->is_admin = $is_admin;
         $this->dbconn = $this->connect();
     }
+    public function getId() {
+        return $this->id;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+    public function getFirstname() {
+        return $this->firstname;
+    }
+    public function setFirstname($firstname) {
+        $this->firstname = $firstname;
+    }
+    public function getLastname() {
+        return $this->lastname;
+    }
+    public function setLastname($lastname) {
+        $this->lastname = $lastname;
+    }
+    public function getUsername() {
+        return $this->username;
+    }
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+    public function getEmail() {
+        return $this->email;
+    }
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+    public function getPassword() {
+        return $this->password;
+    }
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+    public function getIs_Admin() {
+        return $this->is_admin;
+    }
+    public function setIs_Admin($is_admin) {
+        $this->is_admin = $is_admin;
+    }
 }
